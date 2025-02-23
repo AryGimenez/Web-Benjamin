d48806264d4721024d0691c2b7a8f10056866b46
Ary Gimenez <argi.prog@gmail.com>
2025-02-23 11:17:20 -0300

    feat: add mockup and generated web link
    - Added  file to the project root.
    - Included the link to the web generated with the Figma plugin.
    
    feat: agregar mockup y enlace de la web generada
    
    - Agregado el archivo  en la raíz del proyecto.
    - Incluido el enlace a la web generada con el complemento de Figma.

---
+++ MaquetaFigma /data/story.js
@@ -0,0 +1,66 @@
+var story = {
+ "docName": "Mockup Benjamin",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "Mockup Benjamin",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "0:4",
+   "groupIndex": 0,
+   "title": "Home",
+   "image": "home.png",
+   "index": 0,
+   "width": 1440,
+   "height": 4217,
+   "x": -104,
+   "y": -2720,
+   "awidth": 1440,
+   "aheight": 4217,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null
+  }
+ ],
+ "totalImages": 1,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 2",
+   "backColor": "#1E1E1E"
+  },
+  {
+   "id": "177:2",
+   "index": 1,
+   "name": "Page 3",
+   "backColor": "#1E1E1E"
+  },
+  {
+   "id": "177:3",
+   "index": 2,
+   "name": "Page 4",
+   "backColor": "#1E1E1E"
+  }
+ ]
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Proyecto de Imagen: Home.jpg
+
+## Descripción
+
+Este proyecto incluye la imagen `Home.jpg`, que puede ser utilizada para propósitos de diseño, presentación o documentación.
+
+## Contenido
+
+- **Home.jpg:** Imagen principal del proyecto.
+
+## Uso
+
+Puedes utilizar la imagen en tus proyectos de la siguiente manera:
+
+```html
+<img src="Home.jpg" alt="Imagen de Home" width="600">
+```
+
+## Requisitos
+
+Asegúrate de tener la imagen `Home.jpg` en el mismo directorio que el archivo HTML o proyecto donde la utilices.
+
+## Licencia
+
+Este proyecto está bajo la licencia [MIT](https://opensource.org/licenses/MIT).
+
